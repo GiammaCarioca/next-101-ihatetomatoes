1b04cd6a86e1160cb899ff5442a8fad0e6f7ffd5
Thiago Giammattey <giammattey@gmail.com>
2020-12-21 11:58:23 -0300

    add Layout and Hero components, _document and custom colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,19 @@
 import Head from 'next/head'
+import Layout from './components/Layout'
+import Hero from './components/Hero'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Next.js 101</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-
-      <main className='min-h-screen flex flex-col justify-center'>
-        <img
-          className='m-auto my-4 w-24 sm:w-auto'
-          alt='Ihatetomatoes'
-          src='/assets/img_logo.svg'
-        />
-        <h1 className='mx-auto text-4xl font-bold'>Welcome to Next.js 101!</h1>
-      </main>
-    </div>
+      <Layout>
+        <main className='p-4 bg-white'>
+          <Hero />
+        </main>
+      </Layout>
+    </>
   )
 }
